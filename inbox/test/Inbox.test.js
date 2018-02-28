dac4babd544e5b66639031c5bf91ecd9ed3998ca
christinto <chrisperrymedia@gmail.com>
2018-02-28 13:03:20 +1100

    unit tests added

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -1,5 +1,31 @@
 const assert = require('assert');
-const ganache = require()
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+const web3 = new Web3(ganache.provider());
+
+//whenever someone calls park, we will return stopped..
+//basic unit test, success 28/2/18 :)
+class Car {
+    park() {
+    return 'stopped'}
+
+
+    drive() {
+    return 'vroom';
+    }
+}
+
+//pass in string of Car, just to have something for testing report.. could be CarClass..anything we want
+//2nd argument will be an arrow function, contains all the different it test statements. Could have one or more it statements, sring describes purpose of test.. 2nd argument another arrow function sets up actual test setup and assertion logic. Make sure that when we call park the string stopped is called. so we need to create an instance of Car. Then write an assertion that we're returning the word stopped. Use assert library, part of standard node library.. write out value produced by our code.. car.park... then assert value we think it should be.. 'stopped'
+// assert.equal tries to make sure that the code is both equal. to make mocha run in our file... run a script in package.json file..
+// in package.json... tests: mocha
+//npm run test...
+describe('Car', () => {
+    it('can park', () => {
+    const car = new Car();
+    assert.equal(car.park(), 'stopped');
+    });
+})
 
 /*3 main functions we need to be aware of...
  it        - to run a test and make an assertion, take 2 values..one our code has produced and one we think it should be..
