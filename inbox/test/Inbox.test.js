0133ffb44a778f6c1c50e1c35a73615959fc022e
christinto <chrisperrymedia@gmail.com>
2018-02-28 12:42:22 +1100

    notes added from udemy vids

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -1,12 +1,28 @@
 const assert = require('assert');
+const ganache = require()
+
+/*3 main functions we need to be aware of...
+ it        - to run a test and make an assertion, take 2 values..one our code has produced and one we think it should be..
+ describe  - groups together a collection of 'it' functions
+ beforeEach -  executes some general setup code
+
+
+/* Git clone code..
+
+const assert = require('assert');
 const ganache = require('ganache-cli');
+// as Web3 is a constructor function, by tradition we make it capital W.
 const Web3 = require('web3');
+//assign instance to lowercase web3, tells instance to connect to local test network..
+//replace provider with a different provider like rinkeby etc when you wanna change networks
+//interface is abi, bytecode is the raw data
 const web3 = new Web3(ganache.provider());
 const { interface, bytecode } = require('../compile');
 
 let accounts;
 let inbox;
 
+//using async await syntax instead of .then all the time
 beforeEach(async () => {
   // Get a list of all accounts
   accounts = await web3.eth.getAccounts();
@@ -37,3 +53,4 @@ describe('Inbox', () => {
     assert.equal(message, 'bye');
   });
 });
+*/
